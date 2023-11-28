bd4c6e1f571922438060a4c4127039d323611d75
g-launchpad <guilherme@nexyield.com>
2023-11-28 09:51:27 +0000

    fix deprecated postcss dependency

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,55 +1,72 @@
-import { dirname, join } from "path";
+import { dirname, join } from 'path';
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
 
-  staticDirs: ["../images"],
+  staticDirs: ['../images'],
 
   webpackFinal: (config) => {
     // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
     const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test && rule.test.test(".svg")
+      (rule) => rule.test && rule.test.test('.svg')
     );
     fileLoaderRule.exclude = /\.svg$/;
 
     config.module.rules.push({
       test: /\.svg$/,
-      enforce: "pre",
-      loader: require.resolve("@svgr/webpack"),
+      enforce: 'pre',
+      loader: require.resolve('@svgr/webpack'),
     });
 
     return config;
   },
 
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-a11y"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-a11y'),
     {
-      name: "@storybook/addon-postcss",
+      name: '@storybook/addon-styling-webpack',
       options: {
-        postcssLoaderOptions: {
-          implementation: require("postcss"),
-        },
+        // Replaces existing CSS rules with given rule
+        rules: [
+          // Replaces existing CSS rules to support PostCSS
+          {
+            test: /\.css$/,
+            use: [
+              'style-loader',
+              {
+                loader: 'css-loader',
+                options: { importLoaders: 1 },
+              },
+              {
+                // Gets options from `postcss.config.js` in your project root
+                loader: 'postcss-loader',
+                options: { implementation: require.resolve('postcss') },
+              },
+            ],
+          },
+        ],
       },
     },
-    getAbsolutePath("@storybook/addon-mdx-gfm")
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
+    getAbsolutePath('@storybook/addon-mdx-gfm'),
   ],
 
   framework: {
-    name: getAbsolutePath("@storybook/nextjs"),
-    options: {}
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {},
   },
 
   typescript: { reactDocgen: false },
 
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
 
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
